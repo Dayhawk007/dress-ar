b29c086ce3d11fc18dee9e8db433827f64c41f9b
Dayhawk <ayushchandwani26@gmail.com>
2023-11-03 13:20:05 +0530

    updated final positioning

--- src/components/ClothThreeFiber.jsx
+++ src/components/ClothThreeFiber.jsx
@@ -12,15 +12,18 @@ const ClothThreeFiber = (props) => {
     const [offsetVector, setOffsetVector] = useState(new Vector3(0,0,0));
     useEffect(() => {
         if(props.rightShoulder && props.leftShoulder){
-            const x=(((props.leftShoulder.x+props.rightShoulder.x)/2)-320)/106.66;
+            const x=(((props.leftShoulder.x+props.rightShoulder.x)/2)-280)/106.66;
+            const y=(((props.leftShoulder.y+props.rightShoulder.y)/2)-240)/80.0;
             setXOffset(x);
+            setYOffset(y);
+            console.log(x,y);
         }
     }, [props]);
     return (
         <Canvas>
         <Suspense fallback={null}>
             <ambientLight intensity={5} position={[0,-20,0]} />
-            <Model position={[xOffSet,-12,0]} scale={props.eyeDistance/25.0}   />
+            <Model position={[xOffSet,-13.5+yOffSet,-7+(props.eyeDistance/25.0)]} scale={8}   />
         </Suspense>
         </Canvas>
     );
